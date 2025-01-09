10dbadb402955e10e9acdc954cbd1bc6887f2491
k <162461123+4UT15T1Ck@users.noreply.github.com>
2025-01-09 21:30:00 +0700

    combine to 1 function

--- pokemon.js
+++ pokemon.js
@@ -16,33 +16,28 @@ const poke_type = [
 ];
 const container = document.querySelector(".pokemon-container");
 
-function card_content(index) {
-    let d_index = Number(index) + 1;
-    const child = document.createElement("div");
-    child.classList.add("pokemon-card");
-    const pokemon_index = document.createElement("p");
-    pokemon_index.innerText = `#${d_index}`;
-    const poke_img = document.createElement("img");
-    poke_img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${d_index}.png`;
-    poke_img.alt = poke_name[index];
-    const pokemon_name = document.createElement("div");
-    pokemon_name.classList.add("name");
-    child.appendChild(pokemon_index);
-    child.appendChild(poke_img);
-    child.appendChild(pokemon_name);
-    for (type of poke_type[index]) {
-        const newType = document.createElement("div");
-        newType.classList.add("type", type);
-        newType.innerText = type;
-        child.appendChild(newType);
-    }
-    return child;
-}
-
 function show_pokemon() {
-    container.innerHTML = ''
-    for (index in poke_name) {
-        const newItem = card_content(index);
-        container.appendChild(newItem);
+    container.innerHTML = "";
+    for (each in poke_name) {
+        let d_each = Number(each) + 1;
+        const child = document.createElement("div");
+        child.classList.add("pokemon-card");
+        const pokemon_each = document.createElement("p");
+        pokemon_each.innerText = `#${d_each}`;
+        const poke_img = document.createElement("img");
+        poke_img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${d_each}.png`;
+        poke_img.alt = poke_name[each];
+        const pokemon_name = document.createElement("div");
+        pokemon_name.classList.add("name");
+        child.appendChild(pokemon_each);
+        child.appendChild(poke_img);
+        child.appendChild(pokemon_name);
+        for (type of poke_type[each]) {
+            const newType = document.createElement("div");
+            newType.classList.add("type", type);
+            newType.innerText = type;
+            child.appendChild(newType);
+        }
+        container.appendChild(child);
     }
 }
